5875a66cd3baa1305001c2c50c7904557bc02100
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-07-01 02:16:00 +0530

    tables created

--- public/db.js
+++ public/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+import bcrypt from 'bcrypt-nodejs'
 
 const db = new Sequelize('bookstore', 'bookseller', 'bookpass', {
     host: 'localhost',
@@ -10,10 +11,105 @@ const db = new Sequelize('bookstore', 'bookseller', 'bookpass', {
 })
 
 
+const User = db.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+                args: [0, 100],
+                msg: 'Number of character should be less than 50'
+            }
+        }
+    },
+    email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: {
+            msg: 'Not a valid email'
+          },
+          isUnique: connection.validateIsUnique(
+            'email',
+            'Already exists.'
+          )
+        }
+      },
+    phoneNumber: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    college: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    }, {
+      instanceMethods: {
+        generateHash: function (password) {
+          return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+        },
+        validPassword: function (password) {
+          return bcrypt.compareSync(password, this.password)
+        }
+      }
+})
+
+const Listing = db.define('listings', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    image: {
+        type: Sequelize.BLOB('long'),
+        allowNull: false
+    },
+    bookname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+              args: [0, 100],
+              msg: 'Number of character should be less than 50'
+            }
+          }
+    },
+    authorname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: {
+                args: [0, 100],
+                msg: 'Number of character should be less than 50'
+            }
+        }
+    },
+    condition: {
+        type: Sequelize.ENUM,
+        values: ['New', 'Almost New', 'Slight damage', 'Worn']
+    }
+})
+
+Listing.belongsTo(User);
+
 db.sync()
     .then(() => console.log("Database has been synced"))
     .catch((err) => console.error("Error creating database"))
 
 exports = module.exports = {
-  
+    User, Listing
 }
